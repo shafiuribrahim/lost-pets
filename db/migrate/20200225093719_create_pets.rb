119f970acbfc2ebfd91d92f65748d0cb40395d6f
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-02-25 17:42:56 +0800

    created model

---
+++ db/migrate/20200225093719_create_pets.rb
@@ -0,0 +1,11 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :address
+      t.string :species
+
+      t.timestamps
+    end
+  end
+end
